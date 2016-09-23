f17926308a65469687548372798b731063f5551f
NielsHolt <niels@steenbuchholt.dk>
2016-09-24 00:29:49 +0200

    Piwik code added

--- dist/fcoo-application.js
+++ dist/fcoo-application.js
@@ -18,23 +18,6 @@
     var ns = window;
 
     /*********************************************************************
-    Set <body> class = 'loading' and adds spinner
-    *********************************************************************/
-    var $body = $('body'),
-        $div  = $('body > div.loading');
-
-    $body.addClass('loading');
-    if (!$div.length){
-      $div = $('<div class="loading"></div>' );
-      $div.prependTo( $body );
-    }
-    $div.append( $('<span class="loading fa fa-circle-o-notch fa-spin fa-2x fa-fw"></span>') );
-
-    $(window).on( 'load', function() { $body.removeClass("loading"); });
-
-
-
-    /*********************************************************************
     Functions to get options from options.application in gruntfile.js
     *********************************************************************/
 
@@ -59,6 +42,21 @@
     };
 
 
+    /*********************************************************************
+    Set <body> class = 'loading' and adds spinner
+    *********************************************************************/
+    var $body = $('body'),
+        $div  = $('body > div.loading');
+
+    $body.addClass('loading');
+    if (!$div.length){
+      $div = $('<div class="loading"></div>' );
+      $div.prependTo( $body );
+    }
+    $div.append( $('<span class="loading fa fa-circle-o-notch fa-spin fa-2x fa-fw"></span>') );
+
+    $(window).on( 'load', function() { $body.removeClass("loading"); });
+
 
     /*********************************************************************
     Initialize raven to report all uncaught exceptions to sentry
@@ -67,6 +65,23 @@
     if (sentryDSN)
         Raven.config(sentryDSN).install();
 
+    /*********************************************************************
+    Adding the Piwik Tracking Code
+    *********************************************************************/
+    var piwikSiteId = ns.getApplicationNumberOption( "{APPLICATION_PIWIKSITEID}", 0);
+    if (piwikSiteId){
+        var _paq = _paq || [];
+        _paq.push(['disableCookies']);
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function() {
+            var u="//analytics.fcoo.dk/piwik/";
+            _paq.push(['setTrackerUrl', u+'piwik.php']);
+            _paq.push(['setSiteId', piwikSiteId]);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+        })();
+    }
 
 
 
