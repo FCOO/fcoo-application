dfe59a685827f36e1fe1b660053c6c1ae51887fd
Niels Holt <niels@steenbuchholt.dk>
2018-04-26 12:07:25 +0200

    Bug fix: Moved code for messages and promise into fcoo-application.js to ensure correct order of execution. Clear up in demo

--- src/fcoo-application.js
+++ src/fcoo-application.js
@@ -15,10 +15,11 @@ Sections:
 5: Initialize offline.js - http://github.hubspot.com/offline/
 6: Initialize raven to report all uncaught exceptions to sentry AND
    Adding the Piwik Tracking Code
-
+7: Set-up jquery-bootstrap-message for different type of messages
+8: Set-up standard error-handler and message for promise
 ****************************************************************************/
 
-(function ($, window/*, document, undefined*/) {
+(function ($, window, Promise/*, document, undefined*/) {
 	"use strict";
 
     /***********************************************************************
@@ -465,8 +466,327 @@ Sections:
 
 */
 
+    /***********************************************************************
+    ************************************************************************
+    7: Set-up jquery-bootstrap-message for different type of messages
+    ************************************************************************
+    ***********************************************************************/
+    //Set icon and name for different message type
+    $.bsNotyIcon = {
+        info        : 'fa-i-info',  //TODO
+        information : 'fa-i-info',  //TODO
+        alert       : 'fa-i-notification',
+        success     : 'fa-i-ok',        //or original 'fa-check',
+        error       : 'fa-i-blocked',   //or original 'fa-ban',
+        warning     : 'fa-i-warning',   //or original 'fa-exclamation-triangle',
+        help        : 'fa-question-circle-o'
+    };
 
-	/******************************************
+    $.bsNotyName = {
+        info        : {da:'Besked', en:'Message'},
+        information : {da:'Besked', en:'Message'},
+        alert       : {da:'Bemærkning', en:'Note'},
+        success     : {da:'Succes', en:'Success'},
+        error       : {da:'Fejl', en:'Error'},
+        warning     : {da:'Advarsel', en:'Warning'},
+        help        : {da:'Hjælp', en:'Help'}
+    };
+
+    //Add plural name
+    $.bsNotyNames = {
+        info        : {da:'Beskeder', en:'Messages'},
+        information : {da:'Beskeder', en:'Messages'},
+        alert       : {da:'Bemærkninger', en:'Notes'},
+        success     : {da:'Succes', en:'Success'},
+        error       : {da:'Fejl', en:'Errors'},
+        warning     : {da:'Advarsler', en:'Warnings'},
+        help        : {da:'Hjælp', en:'Help'}
+    };
+
+
+    //Add 'messages' to fcoo.settings
+    ns.messageStatus = {};
+    ns.settings.add({
+        id          : 'messages',
+        validator   : function(){ return true; },
+        applyFunc   : function( messageStatus ){ ns.messageStatus = messageStatus; },
+        defaultValue: {},
+        callApply   : true
+    });
+
+    //Extend BsMessage with method to get standard FCOO unique id
+    $.BsMessage.prototype.getFCOOId = function(){
+        return 'fcoo_' + this.options.urlId + '_' + this.options.id;
+    };
+
+    /************************************************************
+    messageGroupOptions =
+        default options for all types
+
+    messageGroupTypeOptions[TYPE] =
+        options for message-group of TYPE
+    TYPE = "warning", "help", or "info"
+
+    Defines the options for the tree standard FCOO type of messages
+    'warning'   : Mesages about real-time production
+    'info'      : Typical news about new releases of the application
+    'help'      : Help to the application and generel info a la "About FCOO"
+    **************************************************************/
+
+    var messageGroupOptions = {
+            onStartLoading : function( messageGroup ){
+                //Disable the button while reading data
+                messageGroup.options.$button.addClass('disabled');
+            },
+
+            onErrorLoading : function( messageGroup ){
+                //Save the notyId
+                messageGroup.errorNotyIdList = messageGroup.errorNotyIdList || [];
+                messageGroup.errorNotyIdList.push( ns.lastNotyId );
+            },
+
+            onFinishLoading: function( messageGroup ){
+                //Close all error-noty displayed during loading
+                $.each(messageGroup.errorNotyIdList || [], function(index, notyId){
+                    window.Noty.closeAll(notyId);
+                });
+
+                //Set the header to singular or plural
+                var type = messageGroup.options.type;
+                messageGroup.options.header = {
+                    icon: $.bsNotyIcon[type],
+                    text: messageGroup.getAllStatus().publish == 1 ? $.bsNotyName[type] : $.bsNotyNames[type]
+                };
+
+                //Enable the button after reading data
+                messageGroup.options.$button.removeClass('disabled');
+            },
+
+
+            onChange: function( messageGroup ){
+                var status = messageGroup.getAllStatus(),
+                    $button = messageGroup.options.$button;
+                if (status.publish){
+                    $button
+                        .removeClass('d-none')
+                        .modernizrToggle( 'all-read', !status.unread );
+                    //REMOVED .toggleClass('shake-constant', !!status.unread ); //Makes button shake when there are new messages
+                }
+                else {
+                    //Hide the button if there are no message
+                    $button.addClass('d-none');
+                }
+            }
+
+        },
+
+        messageGroupTypeOptions = {
+
+            //Warning: rapid update and save read-status in sessionStorage
+            warning: {
+                id: 'warning_'+ns.applicationId,
+
+                reloadPeriod: 'PT10M', //Reload every 10 min
+
+                sortBy: 'DATE',
+                sortDesc: true,
+
+                showStatus    : true,
+                showTypeHeader: true,
+                showTypeColor : true,
+                vfFormat      : 'time_local',
+
+                //Save status as sessionStorage
+                loadStatus: function( message ){
+                    return sessionStorage.getItem( message.getFCOOId()  ) == 'READ';
+                },
+                saveStatus: function( message ){
+                    sessionStorage.setItem( message.getFCOOId(), message.options.status ? 'READ' : 'NOTREAD' );
+                },
+            },
+
+            //Info:
+            info: {
+                id: 'info_'+ns.applicationId,
+
+                sortBy    : 'DATE',
+                sortDesc  : true,
+
+                showStatus: true,
+                vfFormat  : 'date_local',
+
+                //Status are loaded from and saved in fcoo.settings under 'messages' as {id: date}
+                loadStatus: function( message ){
+                    ns.settings.get('messages');
+                    return !!ns.messageStatus[message.getFCOOId()];
+                },
+                saveStatus: function( message ){
+                    ns.settings.get('messages');
+                    ns.messageStatus[message.getFCOOId()] = ns.messageStatus[message.getFCOOId()] || moment().format('YYYY-MM-DD');
+                    ns.settings.set('messages', ns.messageStatus);
+                },
+
+            },
+
+            //Help
+            help: {
+                id: 'help_'+ns.applicationId,
+
+                sortBy  : 'INDEX',
+                sortDesc: false,
+            }
+        };
+
+    /************************************************************
+    createFCOOMessageGroup( type, options, $button )
+    Create a message-group showing warning messages
+    Using sessionStorage to save the read-status of the messages
+    **************************************************************/
+    ns.createFCOOMessageGroup = function( type, options, $button ){
+        options =
+            $.extend(
+                {
+                    $button: $button,
+                    type   : type,
+                },
+                messageGroupOptions,
+                messageGroupTypeOptions[type],
+                options
+            );
+
+        var messageGroup = $.bsMessageGroup( options ),
+            setMessageGroupLanguage = function(){
+                messageGroup.setLanguage( ns.settings.get('language') );
+            };
+        setMessageGroupLanguage();
+
+        //Change language in message-group when the global setting change
+        ns.events.on( 'languagechanged', setMessageGroupLanguage );
+
+        $button.on('click', function(){ messageGroup.asBsModal( true ); });
+    };
+
+
+    /***********************************************************************
+    ************************************************************************
+    8: Set-up standard error-handler and message for promise
+    ************************************************************************
+    ***********************************************************************/
+    //Maintain a list of open notys with promise errors. Prevent showing the same error in multi notys
+    var promiseErrorNotys = {};
+
+    //Create a default error-handle. Can be overwritten
+    Promise.defaultErrorHandler = function( reason, url ){
+        var error = Promise.convertReasonToError( reason );
+
+        //Create the content of the error-noty like
+        //"Error"
+        //"Error-message (error-code)"
+        var message =   error.status ?
+                        window.i18next.t( 'error:'+error.status ) :
+                        error.message || '';
+        if (message && (message == error.status))
+            //No status-code or translation => use error.message
+            message = error.message || '';
+
+        //Adjust url to absolute path (very simple)
+        if (url.indexOf('http') == -1){
+            var parts = window.location.href.split('/');
+            parts.pop();
+            url = parts.join('/') + (url.indexOf('/') != 0 ? '/' : '') + url;
+        }
+
+        //Remove any "dummy=ANYTHING" from url to prevent multi error-noty for same url (NOT PRETTY :-) )
+        if (url.indexOf('dummy=') != -1){
+            var newUrl = url.split('dummy=');
+            newUrl.pop();
+            url = newUrl.join('');
+            if ((url.charAt(url.length-1) == '&') || (url.charAt(url.length-1) == '?'))
+                url = url.slice(0, -1);
+        }
+
+        var content = [
+                $('<div class="font-weight-bold"/>').i18n({da:'Fejl', en:'Error'}),
+                $('<span/>').text( message ),
+                error.status ? ' (' + error.status  + ')' : null
+            ],
+            $details = $('<div style="font-family: monospace" class="d-none error-details"><hr></div>'),
+            hasDetails = false,
+            descKey = error.status ? 'error:'+error.status+'-desc' : '',
+            desc = descKey ? window.i18next.t( descKey ) : '';
+
+        if (desc == descKey)
+            desc = '';
+
+        //Create details
+        var details = [
+                {prompt: {da:'Kode', en:'Code'}              , property: error.status },
+                {prompt: 'Url'                               , property: url          },
+                {prompt: {da:'Beskrivelse', en:'Description'}, property: desc         }
+                //TODO Add rest of possible properties (if any?)
+            ];
+
+        $.each( details, function( index, detail ){
+            var content = detail.property || '';
+            if (content){
+                if (hasDetails)
+                    $details.append('<br>');
+                $details.append( $('<span/>').i18n( detail.prompt) );
+                $details.append(': '+content);
+                hasDetails = true;
+            }
+        });
+
+        if (hasDetails)
+            content.push( $details );
+
+        //Create a noty-id to prevent showing same error in more than one noty
+        var notyId = (error.status || '999') +
+                     url.replace(/\//g, "_") +
+                     message.replace(/ /g, "_");
+
+        ns.lastNotyId = notyId;
+
+        //If a noty with same id already existe => flash if!
+        if (promiseErrorNotys[notyId])
+            promiseErrorNotys[notyId].flash();
+        else
+            //If no network connection => flash the noty with "No network connection"-error
+            if (ns.offlineNoty && ns.offlineNoty.shown && !ns.offlineNoty.closed){
+                ns.offlineNoty.flash();
+            }
+            else {
+                //Create a new noty
+                var toggleDetails = function(event){
+                        $(promiseErrorNotys[notyId].barDom).find('.noty-footer a, .error-details').toggleClass('d-none');
+                        event.stopPropagation();
+                    };
+
+                promiseErrorNotys[notyId] = $.bsNoty({
+                    id       : notyId,
+                    type     : 'error',
+
+                    onTop    : true,
+                    onTopLayerClassName: 'noty-on-top',
+
+                    callbacks: { onClose: function(){ promiseErrorNotys[notyId] = null; } },
+                    layout: 'topCenter',
+                    closeWith:['button'],
+                    content: content,
+                    footer: hasDetails ? [
+                                {                    text:{da:'Vis detaljer',   en:'Show details'}, onClick: toggleDetails},
+                                {textClass:'d-none', text:{da:'Skjul detaljer', en:'Hide details'}, onClick: toggleDetails}
+                            ] : null
+                });
+            }
+    };
+
+
+
+
+
+
+    /******************************************
 	Initialize/ready
 	*******************************************/
 	$(function() {
@@ -474,4 +794,4 @@ Sections:
 	});
 	//******************************************
 
-}(jQuery, this, document));
+}(jQuery, this, this.Promise, document));
