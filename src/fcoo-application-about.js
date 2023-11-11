2669ae5e230e8cfcc62534ff4dcf2c603190ca7d
Niels Holt <niels@steenbuchholt.dk>
2023-11-11 02:05:06 +0100

    Application version and build added to "About..."

--- src/fcoo-application-about.js
+++ src/fcoo-application-about.js
@@ -25,7 +25,11 @@ Create and display "About FCOO" info and modal-box
             desc    = i18next.t('desc:owner'),
             address = i18next.t('address:owner'),
             link    = i18next.t('link:owner'),
-            email   = i18next.t('email:owner');
+            email   = i18next.t('email:owner'),
+
+            version         = ns.applicationVersion,
+            build           = ns.applicationBuild,
+            versionAndBuild = version && build ? version + ' / ' + build : null;
 
 
         if (logo != 'owner')
@@ -63,7 +67,6 @@ Create and display "About FCOO" info and modal-box
         if (email == 'owner') email = '';
 
         if (logo || name || desc || link || email)
-// HER>             $content.append('<br>');
             $('<hr/>').addClass('mt-1 mb-1').appendTo( $content );
 
         if (link)
@@ -72,6 +75,12 @@ Create and display "About FCOO" info and modal-box
             $content.append(' - ');
         if (email)
             $content.append( $('<a href="mailto:'+email+'" target="_top">'+email+'</a>') );
+
+        if (versionAndBuild){
+            $('<hr/>').addClass('mt-1 mb-1').appendTo( $content );
+            $('<div/>').addClass('mt-1 mb-1 font-size-0-9em').text(versionAndBuild).appendTo( $content );
+        }
+
 
         $.bsModal({
             noHeader   : true,
